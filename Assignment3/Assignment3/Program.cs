2d5339628d9eea22c45df36d8ef2d0fab3749635
Polinalavender <91316935+Polinalavender@users.noreply.github.com>
2023-02-18 19:15:48 +0100

    Assignment 3

---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,64 @@
+﻿public class BinaryTree
+{
+    public int Value { get; set; }
+    public BinaryTree Left { get; set; }
+    public BinaryTree Right { get; set; }
+
+    public BinaryTree(int value)
+    {
+        Value = value;
+    }
+}
+
+public class BinaryTreeMain
+{
+    public BinaryTree Root { get; set; }
+
+    public void Add(int value)
+    {
+        if (Root == null)
+        {
+            Root = new BinaryTree(value);
+        }
+        else
+        {
+            AddRoot(Root, value);
+        }
+    }
+
+    private void AddRoot(BinaryTree node, int value)
+    {
+        if (value < node.Value)
+        {
+            if (node.Left == null)
+            {
+                node.Left = new BinaryTree(value);
+            }
+            else
+            {
+                AddRoot(node.Left, value);
+            }
+        }
+        else
+        {
+            if (node.Right == null)
+            {
+                node.Right = new BinaryTree(value);
+            }
+            else
+            {
+                AddRoot(node.Right, value);
+            }
+        }
+    }
+
+    public void inOrder(BinaryTree node, Action<int> action)
+    {
+        if (node != null)
+        {
+            inOrder(node.Left, action);
+            action(node.Value);
+            inOrder(node.Right, action);
+        }
+    }
+}
