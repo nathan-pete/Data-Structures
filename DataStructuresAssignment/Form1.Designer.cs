f7e1d780fef9c962f7d84d2077f16099721bfd58
Stef0002 <stefani.margaritova@student.nhlstenden.com>
2023-04-07 00:41:22 +0200

    Loaded back-up

---
+++ DataStructuresAssignment/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace DataStructuresAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.mainLabel = new System.Windows.Forms.Label();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.SearchTextBox = new System.Windows.Forms.TextBox();
+            this.searchSongName = new System.Windows.Forms.Label();
+            this.AddRowButton = new System.Windows.Forms.Button();
+            this.RemoveRowButton = new System.Windows.Forms.Button();
+            this.LinearSearchButton = new System.Windows.Forms.Button();
+            this.LinearSearchTextBox = new System.Windows.Forms.TextBox();
+            this.searchStreamsLabel = new System.Windows.Forms.Label();
+            this.bucketSortButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 114);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(970, 400);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // mainLabel
+            // 
+            this.mainLabel.AutoSize = true;
+            this.mainLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainLabel.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.mainLabel.Location = new System.Drawing.Point(380, 42);
+            this.mainLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.mainLabel.Name = "mainLabel";
+            this.mainLabel.Size = new System.Drawing.Size(296, 37);
+            this.mainLabel.TabIndex = 1;
+            this.mainLabel.Text = "Data about songs ";
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.BackColor = System.Drawing.Color.DarkOrchid;
+            this.SearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchButton.ForeColor = System.Drawing.Color.White;
+            this.SearchButton.Location = new System.Drawing.Point(1088, 212);
+            this.SearchButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(256, 42);
+            this.SearchButton.TabIndex = 2;
+            this.SearchButton.Text = "Song Binary Search";
+            this.SearchButton.UseVisualStyleBackColor = false;
+            this.SearchButton.Click += new System.EventHandler(this.SearchBwutton_Click);
+            // 
+            // SearchTextBox
+            // 
+            this.SearchTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SearchTextBox.Location = new System.Drawing.Point(1088, 155);
+            this.SearchTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.SearchTextBox.Name = "SearchTextBox";
+            this.SearchTextBox.Size = new System.Drawing.Size(322, 31);
+            this.SearchTextBox.TabIndex = 3;
+            // 
+            // searchSongName
+            // 
+            this.searchSongName.AutoSize = true;
+            this.searchSongName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchSongName.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.searchSongName.Location = new System.Drawing.Point(1083, 114);
+            this.searchSongName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.searchSongName.Name = "searchSongName";
+            this.searchSongName.Size = new System.Drawing.Size(353, 26);
+            this.searchSongName.TabIndex = 4;
+            this.searchSongName.Text = "Enter a name of song for search";
+            // 
+            // AddRowButton
+            // 
+            this.AddRowButton.Location = new System.Drawing.Point(38, 570);
+            this.AddRowButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.AddRowButton.Name = "AddRowButton";
+            this.AddRowButton.Size = new System.Drawing.Size(100, 50);
+            this.AddRowButton.TabIndex = 5;
+            this.AddRowButton.Text = "Add row";
+            this.AddRowButton.UseVisualStyleBackColor = true;
+            this.AddRowButton.Click += new System.EventHandler(this.AddRowButton_Click);
+            // 
+            // RemoveRowButton
+            // 
+            this.RemoveRowButton.Location = new System.Drawing.Point(183, 570);
+            this.RemoveRowButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.RemoveRowButton.Name = "RemoveRowButton";
+            this.RemoveRowButton.Size = new System.Drawing.Size(171, 50);
+            this.RemoveRowButton.TabIndex = 6;
+            this.RemoveRowButton.Text = "Remove row";
+            this.RemoveRowButton.UseVisualStyleBackColor = true;
+            this.RemoveRowButton.Click += new System.EventHandler(this.RemoveRowButton_Click);
+            // 
+            // LinearSearchButton
+            // 
+            this.LinearSearchButton.BackColor = System.Drawing.Color.DarkOrchid;
+            this.LinearSearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LinearSearchButton.ForeColor = System.Drawing.Color.White;
+            this.LinearSearchButton.Location = new System.Drawing.Point(1088, 508);
+            this.LinearSearchButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.LinearSearchButton.Name = "LinearSearchButton";
+            this.LinearSearchButton.Size = new System.Drawing.Size(285, 42);
+            this.LinearSearchButton.TabIndex = 8;
+            this.LinearSearchButton.Text = "Streams Linear Search";
+            this.LinearSearchButton.UseVisualStyleBackColor = false;
+            this.LinearSearchButton.Click += new System.EventHandler(this.LenearSearchButton_Click);
+            // 
+            // LinearSearchTextBox
+            // 
+            this.LinearSearchTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LinearSearchTextBox.Location = new System.Drawing.Point(1088, 456);
+            this.LinearSearchTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.LinearSearchTextBox.Name = "LinearSearchTextBox";
+            this.LinearSearchTextBox.Size = new System.Drawing.Size(322, 31);
+            this.LinearSearchTextBox.TabIndex = 9;
+            // 
+            // searchStreamsLabel
+            // 
+            this.searchStreamsLabel.AutoSize = true;
+            this.searchStreamsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchStreamsLabel.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.searchStreamsLabel.Location = new System.Drawing.Point(1083, 411);
+            this.searchStreamsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.searchStreamsLabel.Name = "searchStreamsLabel";
+            this.searchStreamsLabel.Size = new System.Drawing.Size(472, 26);
+            this.searchStreamsLabel.TabIndex = 10;
+            this.searchStreamsLabel.Text = "Enter a streams number for jump searching";
+            // 
+            // bucketSortButton
+            // 
+            this.bucketSortButton.Location = new System.Drawing.Point(738, 565);
+            this.bucketSortButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.bucketSortButton.Name = "bucketSortButton";
+            this.bucketSortButton.Size = new System.Drawing.Size(270, 61);
+            this.bucketSortButton.TabIndex = 11;
+            this.bucketSortButton.Text = "BucketSortButton";
+            this.bucketSortButton.UseVisualStyleBackColor = true;
+            this.bucketSortButton.Click += new System.EventHandler(this.BucketSortButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(431, 565);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 61);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "StreamSortButton";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.StreamSortButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1924, 805);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bucketSortButton);
+            this.Controls.Add(this.searchStreamsLabel);
+            this.Controls.Add(this.LinearSearchTextBox);
+            this.Controls.Add(this.LinearSearchButton);
+            this.Controls.Add(this.RemoveRowButton);
+            this.Controls.Add(this.AddRowButton);
+            this.Controls.Add(this.searchSongName);
+            this.Controls.Add(this.SearchTextBox);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.mainLabel);
+            this.Controls.Add(this.dataGridView1);
+            this.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = " (";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button1;
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label mainLabel;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.TextBox SearchTextBox;
+        private System.Windows.Forms.Label searchSongName;
+        private System.Windows.Forms.Button AddRowButton;
+        private System.Windows.Forms.Button RemoveRowButton;
+        private System.Windows.Forms.Button LinearSearchButton;
+        private System.Windows.Forms.TextBox LinearSearchTextBox;
+        private System.Windows.Forms.Label searchStreamsLabel;
+        private System.Windows.Forms.Button bucketSortButton;
+    }
+}
---
+++ DataStructuresAssignment/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DataStructuresAssignment
+{
+    public partial class Form1 : Form
+
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string filePath = Path.Combine(Application.StartupPath, "Streams.csv");
+            List<string[]> data = LoadCSV(filePath);
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.Columns.Add("Song", "Song");
+            dataGridView1.Columns.Add("Artist", "Artist");
+            dataGridView1.Columns.Add("Streams (Billions)", "Streams (Billions)");
+            dataGridView1.Columns.Add("Release Date", "Release Date");
+
+            //data = ShellSort(data);
+
+            dataGridView1.Rows.Clear();
+            foreach (string[] row in data)
+            {
+                dataGridView1.Rows.Add(row);
+            }
+        }
+        private List<string[]> LoadCSV(string filePath)
+        {
+            List<string[]> data = new List<string[]>();
+            using (var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(',');
+                    data.Add(values);
+                }
+            }
+
+            // Sort the data by the song name
+            data.Sort((x, y) => string.Compare(x[0], y[0], StringComparison.OrdinalIgnoreCase));
+
+            return data;
+        }
+
+
+        private void SearchBwutton_Click(object sender, EventArgs e)
+        {
+            string searchTerm = SearchTextBox.Text.Trim();
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                string filePath = Path.Combine(Application.StartupPath, "Streams.csv");
+                List<string[]> data = LoadCSV(filePath);
+
+                string[] result = BinarySearch(data, searchTerm);
+
+                if (result != null)
+                {
+                    MessageBox.Show($"Song: {result[0]}\nArtist: {result[1]}\nStreams (Billions): {result[2]}\nRelease Date: {result[3]}");
+                }
+                else
+                {
+                    MessageBox.Show("Not found");
+                }
+            }
+        }
+
+        private string[] BinarySearch(List<string[]> data, string searchTerm)
+        {
+            string[] result = null;
+
+            int left = 0;
+            int right = data.Count - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                int compareResult = string.Compare(data[mid][0], searchTerm, StringComparison.OrdinalIgnoreCase);
+
+                if (compareResult == 0)
+                {
+                    result = data[mid];
+                    break;
+                }
+                else if (compareResult < 0)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return result;
+        }
+        private void AddRowButton_Click(object sender, EventArgs e)
+        {
+
+            string[] newRow = new string[] { "New Song", "New Artist", "0.5", "2023-03-24" };
+            string filePath = Path.Combine(Application.StartupPath, "Streams.csv");
+            List<string[]> data = LoadCSV(filePath);
+            data.Add(newRow);
+
+
+            dataGridView1.Rows.Clear();
+            foreach (string[] row in data)
+            {
+                dataGridView1.Rows.Add(row);
+            }
+
+            using (var writer = new StreamWriter(filePath, append: true))
+            {
+                writer.WriteLine(string.Join(",", newRow));
+            }
+        }
+
+        /*----------------------------------------------- Remove Row -------------------------------------------------*/
+        private void RemoveRowButton_Click(object sender, EventArgs e)
+        {
+
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int selectedIndex = dataGridView1.SelectedRows[0].Index;
+                dataGridView1.Rows.RemoveAt(selectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to remove.");
+            }
+        }
+
+        /*----------------------------------------------- Bucket Sort ------------------------------------------------*/
+        private void BucketSortButton_Click(object sender, EventArgs e)
+        {
+            string filePath = Path.Combine(Application.StartupPath, "Streams.csv");
+            List<string[]> data = LoadCSV(filePath);
+
+            // Create a dictionary to store the buckets
+            Dictionary<string, List<string[]>> buckets = new Dictionary<string, List<string[]>>();
+
+            // Iterate over each row in the data and add it to the appropriate bucket
+            foreach (string[] row in data)
+            {
+                string artist = row[1];
+                if (buckets.ContainsKey(artist))
+                {
+                    buckets[artist].Add(row);
+                }
+                else
+                {
+                    buckets.Add(artist, new List<string[]> { row });
+                }
+            }
+
+            // Sort the buckets by artist name
+            List<string[]> sortedData = new List<string[]>();
+            foreach (var bucket in buckets.OrderBy(x => x.Key))
+            {
+                sortedData.AddRange(bucket.Value);
+            }
+
+            // Clear the data grid view and add the sorted data
+            dataGridView1.Rows.Clear();
+            foreach (string[] row in sortedData)
+            {
+                dataGridView1.Rows.Add(row);
+            }
+        }
+
+        /*---------------------------------------------- Linear Sort -------------------------------------------------*/
+        private void LenearSearchButton_Click(object sender, EventArgs e)
+        {
+            string searchTerm = LinearSearchTextBox.Text;
+
+            foreach (DataGridViewRow currentRow in dataGridView1.Rows) // iterate through every row one row at a time
+            {
+                string cellValue = currentRow.Cells["Streams (Billions)"].Value.ToString(); //cell value of the row streams
+
+                if (cellValue.Equals(searchTerm)) // compare cell value of the current row to the search term
+                {
+                    currentRow.Selected = true; //highlight the row of the search term
+                    dataGridView1.CurrentCell = currentRow.Cells[0];
+                    MessageBox.Show($"Song: {currentRow.Cells[0].Value.ToString()}\nArtist: {currentRow.Cells[1].Value.ToString()}\nStreams (Billions): {currentRow.Cells[2].Value.ToString()}\nRelease Date: {currentRow.Cells[3].Value.ToString()}"); //display whole row of the search term
+                    break;
+                }
+                else
+                {
+                    MessageBox.Show("The search result wasn't found, try again.");
+                    break;
+                }
+
+            }
+        }
+
+        /*---------------------------------------------- Bubble Sort -------------------------------------------------*/
+        private void StreamSortButton_Click(object sender, EventArgs e)
+        {
+            //Reads and store CSV data
+            List<string> lines = new List<string>();
+            //StreamReader reads the file and dispose of it when done
+            using (StreamReader reader = new StreamReader("Streams.csv"))
+            {
+                //Saves CSV contents to a lists
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(','); //Splits at the delimiter
+                    lines.Add(values[2]); //Adds 3rd element to the list (Streams column)
+                }
+            }
+
+            // Convert the strings to floats
+            float[] data = new float[lines.Count];
+            for (int x = 0; x < lines.Count; x++) //Iterates through the list
+            {
+                data[x] = float.Parse(lines[x]); //Converts the string to a float
+            }
+
+            // Bubble sort Algorithm
+            for (int x = 0; x < data.Length - 1; x++)
+            {
+                for (int y = 0; y < data.Length - x - 1; y++)
+                {
+                    if (data[y] > data[y + 1])
+                    {
+                        float temp = data[y];
+                        data[y] = data[y + 1];
+                        data[y + 1] = temp;
+                    }
+                }
+            }
+
+            // Rearrange the WinForm table
+            for (int x = 0; x < dataGridView1.Rows.Count & x < data.Length; x++)
+            {
+                dataGridView1.Rows[x].Cells[2].Value = data[x]; //Sets the 3rd column to the sorted data
+            }
+        }
+    }
+}
+
